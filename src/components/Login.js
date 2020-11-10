44af72b0342ab3130203c2a5b4b6ac09d744a4cf
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-11-10 07:19:40 -0700

    testing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './HomePage';
+import HomePage from './Homepage';
 class Login extends React.Component {
     constructor(props){
         super(props);
