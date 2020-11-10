b53b07bdd0ba3f0fb2658d4931171ce216608abc
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-11-10 07:11:50 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter, 
-  Switch, 
-  Route
-} from "react-router-dom";
-import Homepage from './components/Homepage';
 import Login from './components/Login';
-import './components/Website.css';
-
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-    
+    <div className="App">
+      <Login />
+    </div>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import './Website.css';
+import HomePage from './HomePage';
+import './homePage.css';
 class Login extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             username: '',
-            authenticated: false,
+            showHomePage: false,
+            signedIn: false
         };
 
     }
 
     onSubmit = (event) => {
         if(this.state.username.trim().length > 0){
-            this.setState({authenticated: true});
+            this.setState({signedIn: true});
         }
         event.preventDefault();
+        
+
     }
     
-    onInputChange = (event) =>{
+    handleChange = (event) =>{
         const value = event.target.value;
         const name = event.target.name;
         this.setState({
@@ -29,32 +32,38 @@ class Login extends React.Component {
   
 
     render(){
-        let from = {pathname: '/', state: {user: this.state.username}}
+        if((!this.state.signedIn) && (!this.state.showHomePage)){
+            return (
+                <div>
+                    <form className="onlyForm" onSubmit={this.onSubmit}>
+                        <label>Username:</label>
+                        <input 
+                            type="text" 
+                            name="username"
+                            value={this.state.username} 
+                            onChange={this.handleChange}
+                        ></input>
+                        <button className="loginButton" type="submit">Login</button>                
+                        
+                    </form>
+                    
+                </div>
+    
+            );
 
-        if(this.state.authenticated){
-            return(<Redirect to={from} />);
         }
-        else{
         return (
             <div>
-                <form className="onlyForm" onSubmit={this.onSubmit}>
-                    <label>Username:</label>
-                    <input 
-                        type="text" 
-                        name="username"
-                        value={this.state.username} 
-                        onChange={this.onInputChange}
-                    ></input>
-                    <button className="loginButton" type="submit">Login</button>                
-                    
-                </form>
-                
+                <div className="greeting">
+                <p>Welcome  </p>
+                <p>{this.state.username}</p>  
+                <p>!</p>
+
+                </div>
+                <HomePage />
             </div>
 
         );
-        }
-    
-    
     }
 
 }
