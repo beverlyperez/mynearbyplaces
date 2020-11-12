60870936a7c5fcb23590999a471ecd573aaac921
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-11-12 11:41:36 -0700

    added place

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Login from './components/Login';
+import Homepage from './components/Homepage';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import Place from './components/Place';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route exact path='/' render={props => <Homepage {...props} />}>
+
+      </Route>
+      <Route path='/login' >
+        <Login />
+      </Route>
+      <Route path='/place' >
+        <Place />
+      </Route>
+    </Switch>
+  </BrowserRouter>
   );
 }
 
